aa8bff7a8d5313b0d80de1318d680dda1f83e9fe
jimstory <>
2018-09-13 14:49:17 +0800

    fixed poweredby

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -24,7 +24,7 @@ class PaymentController extends Controller
         return app('alipay')->web([
             'out_trade_no' => $order->no, // 订单编号，需保证在商户端不重复
             'total_amount' => $order->total_amount, // 订单金额，单位元，支持小数点后两位
-            'subject'      => '支付 Laravel Shop 的订单：'.$order->no, // 订单标题
+            'subject'      => '支付 Sigma Shop 的订单：'.$order->no, // 订单标题
         ]);
     }
 
@@ -39,7 +39,7 @@ class PaymentController extends Controller
         $wechatOrder = app('wechat_pay')->scan([
             'out_trade_no' => $order->no,
             'total_fee'    => $order->total_amount * 100,
-            'body'         => '支付 Laravel Shop 的订单：'.$order->no,
+            'body'         => '支付 Sigma Shop 的订单：'.$order->no,
         ]);
         // 把要转换的字符串作为 QrCode 的构造函数参数
         $qrCode = new QrCode($wechatOrder->code_url);
--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="footer">
     <div class="container">
         <p class="pull-left">
-            由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Leo</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+            Powered by <a href="http://sigmadata.xyz" target="_blank">SigmaData</a>
         </p>
 
-        <p class="pull-right"><a href="mailto:root@leo108.com">联系我们</a></p>
+        <p class="pull-right"><a href=""></a></p>
     </div>
 </footer>
